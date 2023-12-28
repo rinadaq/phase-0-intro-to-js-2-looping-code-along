5008d053b40583bf4dd1164861258cd8e54bef04
Rinad Abu Qauod <rinadaq98@gmail.com>
2023-12-28 18:49:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// helpers.js
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Export functions for testing
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
